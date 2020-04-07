af38c6c57f9da391bee41ab65bd4f87c8e4386d5
Zoey Chesny <z.chesny@gmail.com>
2020-04-07 21:53:45 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,4 +1,19 @@
 # code your #valid_move? method here
+def valid_move?(board, index)
+  # is index present in game board
+  if position_taken?(board, index)
+
+  end
+
+  # is index not already filled with a token
+end
 
 
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
+def position_taken(board, index)
+  if board[index] == " " || board[index] == "" || board[index] = nil
+    return false
+  else
+    return true
+  end
+end
